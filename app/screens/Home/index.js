7baa84a06724aa6badd0fc29f84321ab9b2eab13
Changhwan Choi <changhwanchoi@Changhwanui-iMac.local>
2020-09-29 08:55:50 +0900

    first

---
+++ app/screens/Home/index.js
@@ -0,0 +1,358 @@
+import React, {useState} from 'react';
+import {
+  View,
+  ScrollView,
+  Animated,
+  TouchableOpacity,
+  FlatList,
+  Dimensions,
+} from 'react-native';
+import {
+  Image,
+  Text,
+  Icon,
+  HotelItem,
+  Card,
+  Button,
+  SafeAreaView,
+  EventCard,
+  TextInput
+} from '@components';
+import {BaseStyle, Images, useTheme} from '@config';
+import * as Utils from '@utils';
+import styles from './styles';
+import {PromotionData, TourData, HotelData} from '@data';
+import {useTranslation} from 'react-i18next';
+
+export default function Home({navigation}) {
+  const {t} = useTranslation();
+  const {colors} = useTheme();
+  const [icons] = useState([
+    {
+      icon: 'calendar-alt',
+      name: '저녁파티',
+      route: 'Test',
+    },
+    {
+      icon: 'map-marker-alt',
+      name: '액티비티',
+      route: 'Tour',
+    },
+    {
+      icon: 'car-alt',
+      name: '이색체험투어',
+      route: 'OverViewCar',
+    },
+    {
+      icon: 'plane',
+      name: 'flight',
+      route: 'FlightSearch',
+    },
+    // {
+    //   icon: 'ship',
+    //   name: 'cruise',
+    //   route: 'CruiseSearch',
+    // },
+    // {
+    //   icon: 'bus',
+    //   name: 'bus',
+    //   route: 'BusSearch',
+    // },
+    // {
+    //   icon: 'star',
+    //   name: 'event',
+    //   route: 'DashboardEvent',
+    // },
+    // {
+    //   icon: 'ellipsis-h',
+    //   name: 'more',
+    //   route: 'More',
+    // },
+  ]);
+  const [relate] = useState([
+    {
+      id: '0',
+      image: Images.event4,
+      title: 'BBC Music Introducing',
+      time: 'Thu, Oct 31, 9:00am',
+      location: 'Tobacco Dock, London',
+    },
+    {
+      id: '1',
+      image: Images.event5,
+      title: 'Bearded Theory Spring Gathering',
+      time: 'Thu, Oct 31, 9:00am',
+      location: 'Tobacco Dock, London',
+    },
+  ]);
+  const [promotion] = useState(PromotionData);
+  const [tours] = useState(TourData);
+  const [hotels] = useState(HotelData);
+  const [heightHeader, setHeightHeader] = useState(Utils.heightHeader());
+  const deltaY = new Animated.Value(0);
+
+  /**
+   * @description Show icon services on form searching
+   * @author Passion UI <passionui.com>
+   * @date 2019-08-03
+   * @returns
+   */
+  const renderIconService = () => {
+    return (
+      <FlatList
+        data={icons}
+        numColumns={4}
+        keyExtractor={(item, index) => index.toString()}
+        renderItem={({item}) => {
+          return (
+            <TouchableOpacity
+              style={styles.itemService}
+              activeOpacity={0.9}
+              onPress={() => {
+                navigation.navigate(item.route);
+              }}>
+              <View
+                style={[styles.iconContent, {backgroundColor: colors.card}]}>
+                <Icon name={item.icon} size={18} color={colors.primary} solid />
+              </View>
+              <Text footnote grayColor numberOfLines={1}>
+                {t(item.name)}
+              </Text>
+            </TouchableOpacity>
+          );
+        }}
+      />
+    );
+  };
+
+  const heightImageBanner = Utils.scaleWithPixel(265);
+  const marginTopBanner = heightImageBanner - heightHeader;
+
+  return (
+    <View style={{flex: 1}}>
+      <View style={[
+          styles.new_top_bar,
+        ]}>
+          <View>
+            <Image source={Images.logo} style={styles.new_top_logo} />
+          </View>
+        <View style={[styles.new_top_bar_input_wrapper,]}>
+          <TextInput 
+          style={[styles.new_top_textinput,]}
+            placeholder={t('플리야 어디갈꺼야?')}
+          />
+          <Image source={Images.new_icon_01_search} style={styles.new_top_textinput_icon} />
+        </View>
+      </View>
+      <Animated.Image
+        source={Images.new_main_banner}
+        style={[
+          styles.imageBackground,
+          {
+            height: deltaY.interpolate({
+              inputRange: [
+                0,
+                Utils.scaleWithPixel(100),
+                Utils.scaleWithPixel(100),
+              ],
+              outputRange: [heightImageBanner, heightHeader, 0],
+            }),
+          },
+        ]}
+      />
+      <SafeAreaView style={{flex: 1}} forceInset={{top: 'always'}}>
+        <ScrollView
+          onScroll={Animated.event([
+            {
+              nativeEvent: {
+                contentOffset: {y: deltaY},
+              },
+            },
+          ])}
+          onContentSizeChange={() => setHeightHeader(Utils.heightHeader())}
+          scrollEventThrottle={8}>
+          <View>
+            <View style={[styles.first_button_wrapper]}>
+              <TouchableOpacity style={[styles.first_button]}>
+                <Image source={Images.new_icon_08} style={styles.first_button_image} />
+                <Text style={[styles.first_button_text]}>저녁파티</Text>
+              </TouchableOpacity>
+              <TouchableOpacity style={[styles.first_button]}>
+                <Image source={Images.new_icon_07} style={styles.first_button_image} />
+                <Text style={[styles.first_button_text]}>액티비티</Text>
+              </TouchableOpacity>
+              <TouchableOpacity style={[styles.first_button]}>
+                <Image source={Images.new_icon_09} style={styles.first_button_image} />
+                <Text style={[styles.first_button_text]}>이색체험·투어</Text>
+              </TouchableOpacity>
+              <TouchableOpacity style={[styles.first_button]}>
+                <Image source={Images.new_icon_06} style={styles.first_button_image} />
+                <Text style={[styles.first_button_text]}>관광지</Text>
+              </TouchableOpacity>
+              <TouchableOpacity style={[styles.first_button]}>
+                <Image source={Images.new_icon_05} style={styles.first_button_image} />
+                <Text style={[styles.first_button_text]}>숙박</Text>
+              </TouchableOpacity>
+            </View>
+            {/* <View
+              style={[
+                styles.searchForm,
+                {
+                  marginTop: 332,
+                  backgroundColor: colors.background,
+                  borderColor: colors.border,
+                  shadowColor: colors.border,
+                },
+              ]}>
+              {renderIconService()}
+            </View> */}
+          </View>
+          <View>
+            <View  style={styles.new_title_big_wrapper}>
+              <View style={styles.new_title_wrapper} >
+                <Text title3 semibold style={styles.titleView}>
+                  {t('플리가 좋아하는 여행모임')}
+                </Text>
+                <Image source={Images.new_icon_04} style={styles.new_title_image} />
+              </View>
+              <Button style={styles.new_button_all}><Text style={styles.new_button_all_text}>모두보기</Text></Button>
+            </View>
+            <FlatList
+              contentContainerStyle={{paddingLeft: 5, paddingRight: 20}}
+              horizontal={true}
+              showsHorizontalScrollIndicator={false}
+              data={promotion}
+              keyExtractor={(item, index) => item.id}
+              renderItem={({item, index}) => (
+                <Card
+                  style={[styles.promotionItem, {marginLeft: 15}]}
+                  image={item.image}
+                  onPress={() => navigation.navigate('HotelDetail')}>
+                  <Text subhead whiteColor>
+                    {item.title1}
+                  </Text>
+                  <Text title2 whiteColor semibold>
+                    {item.title2}
+                  </Text>
+                  <View style={styles.contentCartPromotion}>
+                    <Button
+                      style={styles.btnPromotion}
+                      onPress={() => {
+                        navigation.navigate('PreviewBooking');
+                      }}>
+                      <Text body2 semibold whiteColor>
+                        {t('book_now')}
+                      </Text>
+                    </Button>
+                  </View>
+                </Card>
+              )}
+            />
+          </View>
+          {/* Hiking */}
+          <View style={styles.titleView}>
+            <Text title3 semibold>
+              {t('tours')}
+            </Text>
+            <Text body2 grayColor>
+              {t('let_find_tour')}
+            </Text>
+          </View>
+          <FlatList
+            contentContainerStyle={{paddingLeft: 5, paddingRight: 20}}
+            horizontal={true}
+            showsHorizontalScrollIndicator={false}
+            data={tours}
+            keyExtractor={(item, index) => item.id}
+            renderItem={({item, index}) => (
+              <Card
+                style={[styles.tourItem, {marginLeft: 15}]}
+                image={item.image}
+                onPress={() => navigation.navigate('TourDetail')}>
+                <Text headline whiteColor semibold>
+                  {item.name}
+                </Text>
+              </Card>
+            )}
+          />
+          {/* Event*/}
+          <View style={styles.titleView}>
+            <Text title3 semibold>
+              {t('comming_event')}
+            </Text>
+            <Text body2 grayColor>
+              {t('let_find_event')}
+            </Text>
+          </View>
+          <View>
+            <FlatList
+              contentContainerStyle={{
+                paddingRight: 20,
+                paddingLeft: 5,
+              }}
+              horizontal={true}
+              data={relate}
+              showsHorizontalScrollIndicator={false}
+              keyExtractor={(item, index) => item.id}
+              renderItem={({item, index}) => (
+                <EventCard
+                  image={item.image}
+                  title={item.title}
+                  time={item.time}
+                  location={item.location}
+                  onPress={() => navigation.navigate('EventDetail')}
+                  style={{marginLeft: 15}}
+                />
+              )}
+            />
+          </View>
+          {/* Promotion */}
+          <View style={styles.titleView}>
+            <Text title3 semibold>
+              {t('promotion')}
+            </Text>
+            <Text body2 grayColor>
+              {t('let_find_promotion')}
+            </Text>
+            <Image source={Images.banner1} style={styles.promotionBanner} />
+            <View style={[styles.line, {backgroundColor: colors.border}]} />
+          </View>
+          <FlatList
+            columnWrapperStyle={{paddingLeft: 5, paddingRight: 20}}
+            numColumns={2}
+            data={hotels}
+            keyExtractor={(item, index) => item.id}
+            renderItem={({item, index}) => (
+              <HotelItem
+                grid
+                image={item.image}
+                name={item.name}
+                location={item.location}
+                price={item.price}
+                available={item.available}
+                rate={item.rate}
+                rateStatus={item.rateStatus}
+                numReviews={item.numReviews}
+                services={item.services}
+                style={{marginLeft: 15, marginBottom: 15}}
+                onPress={() => navigation.navigate('HotelDetail')}
+              />
+            )}
+          />
+        </ScrollView>
+      </SafeAreaView>
+    </View>
+  );
+}
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ app/screens/Home/styles.js
@@ -0,0 +1,165 @@
+import {StyleSheet, Dimensions} from 'react-native';
+import * as Utils from '@utils';
+
+const window = Dimensions.get("window");
+const screen = Dimensions.get("screen");
+
+export default StyleSheet.create({
+  imageBackground: {
+    height: 265,
+    width: '100%',
+    position: 'absolute',
+    backgroundColor: '#ffffff',
+    marginTop: 75,
+  },
+  searchForm: {
+    padding: 10,
+    borderRadius: 0,
+    borderWidth: 0,
+    width: '100%',
+    shadowColor: 'black',
+    shadowOffset: {width: 0, height: 0},
+    shadowOpacity: 0,
+    shadowRadius: 0,
+    elevation: 0,
+    borderBottomWidth: 10,
+    borderBottomColor: '#f6f6f6'
+  },
+  contentServiceIcon: {
+    marginTop: 10,
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+  },
+  contentCartPromotion: {
+    marginTop: 10,
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+  },
+  btnPromotion: {
+    height: 25,
+    borderRadius: 3,
+    paddingHorizontal: 10,
+    paddingVertical: 5,
+  },
+  contentHiking: {
+    marginTop: 20,
+    marginLeft: 20,
+    marginBottom: 10,
+  },
+  promotionBanner: {
+    height: Utils.scaleWithPixel(100),
+    width: '100%',
+    marginTop: 10,
+  },
+  line: {
+    height: 1,
+    marginTop: 10,
+    marginBottom: 15,
+  },
+  iconContent: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: 36,
+    height: 36,
+    borderRadius: 18,
+  },
+  itemService: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    flex: 1,
+    paddingTop: 10,
+  },
+  promotionItem: {
+    width: Utils.scaleWithPixel(200),
+    height: Utils.scaleWithPixel(250),
+  },
+  tourItem: {
+    width: Utils.scaleWithPixel(135),
+    height: Utils.scaleWithPixel(160),
+  },
+  titleView: {
+    paddingLeft: 20,
+    paddingTop: 20,
+    paddingBottom: 10,
+    marginRight:1,
+  },
+
+
+
+
+  new_top_bar: {
+    flexDirection: 'row',
+    height: 61, 
+    marginHorizontal: 20,
+    paddingTop: 10
+  },
+  new_top_logo: {
+    height: 51,
+    width: 57,
+    marginRight: 10,
+  },
+  new_top_bar_input_wrapper: {
+    width: screen.width * 1 - 107,
+    height: 46,
+    flexDirection: 'row',
+    borderBottomColor: '#9e9e9e',
+    borderBottomWidth: 1,
+    paddingRight: 10
+  },
+  new_top_textinput: {
+    width: screen.width * 1 - 137,
+    backgroundColor:'white', 
+    borderBottomColor: '#9e9e9e',
+    borderBottomWidth: 1
+  },
+  new_top_textinput_icon: {
+    width:20, height:20,
+    marginTop: 12,
+  },
+  first_button_wrapper: {
+    marginTop: 352,
+    flexDirection: 'row',
+    justifyContent:'space-between',
+    paddingHorizontal: 20,
+    paddingBottom: 20,
+    borderBottomColor: '#f6f6f6',
+    borderBottomWidth: 13
+  },
+  first_button: {
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  first_button_image: {
+    width: 50, height: 50,
+    marginBottom: 10
+  },
+  first_button_text: {
+    fontSize: 12,
+    color: '#656565',
+  },
+  new_title_wrapper: {
+    flexDirection: 'row',
+    alignItems:'center'
+  },
+  new_title_image: {
+    width: 27, height: 27,
+    marginTop: 5
+  },
+  new_button_all: {
+    backgroundColor: '#f7f7f7',
+    borderRadius: 30,
+    width: 80,
+    height: 30,
+    marginTop: 13,
+  },
+  new_button_all_text: {
+    color: '#b6b6b6',
+    fontSize: 11,
+    fontWeight: '300'
+  },
+  new_title_big_wrapper: {
+    flexDirection: 'row',
+    justifyContent:'space-between',
+    paddingRight: 20
+  }
+});
